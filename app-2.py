e16707d56f7fac8386373b63c1a07741a36ca8ab
Rj@Ml029 <ronakjha029@gmail.com>
2026-03-25 15:57:37 +0530

    Update app-2.py

--- app-2.py
+++ app-2.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import joblib
